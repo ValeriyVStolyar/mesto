2a15fd50db5a91dce4b4522d63454ae21bf8bd7c
Valeriy Stolyar <valeriistolyar@yandex.ru>
2021-01-29 01:34:31 +0300

    fix: сердечко актив в JS

--- scripts/heart.js
+++ scripts/heart.js
@@ -1,19 +1,29 @@
-let heartElement = document.querySelector('.element-group');
+let heartActive = document.querySelector('.place__heart');
 
-let actionChange = function(evt) {
-  //if (mouseover) {
-  if (evt.target === evt.currentTarget) {
-    heartElement.innerHTML = `<img src="./images/element-heart-hover.png"
-    alt="Сердечко" class="element-group"></img>`;
-  }
-  //if (mousedown) {
-  if (evt.target !== evt.currentTarget) {
-    heartElement.innerHTML = `<img src="./images/element-heart-active.png"
-    alt="Сердечко" class="element-group"></img>`;
-  }
-  else {
-    heartElement.innerHTML = `<img src="./images/element-heart.png"
-    alt="Сердечко" class="element-group"></img>`;
-  }
+heartActive.addEventListener('click', function(){
+  heartActive.innerHTML = '<div class="place__heart place__heart_active">&#9829;</div>';
+});
+
+let heartsLike = document.querySelectorAll('.place__heart');
+
+function heartsActiveOne() {
+  heartsLike[1].innerHTML = '<div class="place__heart place__heart_active">&#9829;</div>';
 }
-actionChange(evt);
+function heartsActiveTwo() {
+  heartsLike[2].innerHTML = '<div class="place__heart place__heart_active">&#9829;</div>';
+}
+function heartsActiveThree() {
+  heartsLike[3].innerHTML = '<div class="place__heart place__heart_active">&#9829;</div>';
+}
+function heartsActiveFour() {
+  heartsLike[4].innerHTML = '<div class="place__heart place__heart_active">&#9829;</div>';
+}
+function heartsActiveFive() {
+  heartsLike[5].innerHTML = '<div class="place__heart place__heart_active">&#9829;</div>';
+}
+
+heartsLike[1].addEventListener('click', heartsActiveOne);
+heartsLike[2].addEventListener('click', heartsActiveTwo);
+heartsLike[3].addEventListener('click', heartsActiveThree);
+heartsLike[4].addEventListener('click', heartsActiveFour);
+heartsLike[5].addEventListener('click', heartsActiveFive);
