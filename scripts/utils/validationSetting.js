9aa4c91f610c9c673088b1319ddbd9002206dcb0
Valeriy Stolyar <valeriistolyar@yandex.ru>
2021-06-01 13:11:31 +0300

    Updates

--- scripts/utils/validationSetting.js
+++
@@ -1,8 +0,0 @@
-export const validationSetting = ({
-  formSelector: '.popup__validate',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.button_type_submit',
-  inactiveButtonClass: 'button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-});
